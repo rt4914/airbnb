4f9d3e954c5f68cbb81c5e7d1547d2bba5d1a8fa
rt4914 <rajattalesra4914@gmail.com>
2024-01-24 12:56:46 +0530

    nit: minor fixes to wishlist icon

--- app/javascript/controllers/wishlist_controller.js
+++ app/javascript/controllers/wishlist_controller.js
@@ -3,6 +3,16 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   updateWishlistStatus() {
+    // Get status if user is logged-in
+    // If logged-out, redirect to login page, return
+    // else continue
+
+    const isUserLoggedIn = this.element.dataset.userLoggedIn;
+    if(isUserLoggedIn === "false"){
+      document.querySelector(".js-login").click();
+      return
+    }
+
     if (this.element.dataset.status === "false"){
       this.element.classList.remove("fill-none");
       this.element.classList.add("fill-primary");
