d6cdf23a8a05ec5759bcb67335e8d29d4d0a7d91
rt4914 <rajattalesra4914@gmail.com>
2024-02-03 23:13:21 +0530

    backend: overall_ratings

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -2,5 +2,10 @@ class PropertiesController < ApplicationController
 
   def show
     @property = Property.find(params[:id])
+    @overall_rating_counts = @property
+                              .reviews
+                              .group('ROUND(final_rating)')
+                              .count
+                              .transform_keys(&:to_i)
   end
 end
