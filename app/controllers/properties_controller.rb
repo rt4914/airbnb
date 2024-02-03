272ab8752c0bf83adbb4a458fcc432b755d7841f
rt4914 <rajattalesra4914@gmail.com>
2024-02-04 00:11:56 +0530

    minor: optimisation

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,11 +1,13 @@
 class PropertiesController < ApplicationController
 
   def show
-    @property = Property.find(params[:id])
+    @property = Property.includes(:reviews).find(params[:id])
     @overall_rating_counts = @property
                               .reviews
                               .group('ROUND(final_rating)')
                               .count
                               .transform_keys(&:to_i)
+    
+    @overall_rating_counts.default = 0
   end
 end
