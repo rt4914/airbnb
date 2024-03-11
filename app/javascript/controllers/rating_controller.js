cd3f57cc62d9d2fe38f6c90973212156568b105f
rt4914 <rajattalesra4914@gmail.com>
2024-03-11 22:44:13 +0530

    ui: review rating

--- app/javascript/controllers/rating_controller.js
+++ app/javascript/controllers/rating_controller.js
@@ -6,7 +6,20 @@ export default class extends Controller {
     const rating = e.currentTarget.dataset.rating;
     const ratingCategory = e.currentTarget.dataset.category;
     const rootElement = e.currentTarget.parentNode
-    const ratingInput = rootElement.querySelector(`input[data-rating-target="${ratingCategory}_rating"]`); 
+    const ratingInput = rootElement.querySelector(`input[data-rating-target="${ratingCategory}"]`); 
+
+    const stars = rootElement.querySelectorAll(`[data-rating-target="star"]`); 
+
+    stars.forEach(star => {
+      if(star.dataset.rating <= rating){
+        star.classList.remove('fill-gray-300')
+        star.classList.add('fill-amber-300')
+      }
+      else{
+        star.classList.remove('fill-amber-300')
+        star.classList.add('fill-gray-300')
+      }
+    });
 
     if (ratingInput) {
       ratingInput.value = rating;
