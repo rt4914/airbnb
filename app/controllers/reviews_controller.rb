4c1937562f9549ed09670775767f3a43cd97249f
rt4914 <rajattalesra4914@gmail.com>
2024-03-11 20:32:49 +0530

    backend: add review api

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,4 +2,30 @@ class ReviewsController < ApplicationController
   def new
     @reservation = Reservation.find(params[:reservation_id])
   end
+
+  def create
+    @review = Review.new(review_params)
+
+    if @review.save
+      redirect_to root_path, notice: 'Review added successfully'
+    else
+      redirect_back fallback_location: root_path, alert: 'Failed to add review'
+    end
+  end
+
+  private
+
+  def review_params
+    params.permit(
+      :user_id,
+      :property_id,
+      :content,
+      :cleanliness_rating,
+      :accuracy_rating,
+      :checkin_rating,
+      :communication_rating,
+      :location_rating,
+      :value_rating
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,6 @@ Rails.application.routes.draw do
   resources :profiles, only: [:index]
 
   resources :reservations, only: [:index] do
-    resources :reviews, only: [:new]
+    resources :reviews, only: [:new, :create]
   end
 end
