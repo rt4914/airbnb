69a4ddc826ca69da5900571bf8aeafc72ab44ced
rt4914 <rajattalesra4914@gmail.com>
2024-03-23 18:36:50 +0530

    owner property edit partial

--- app/controllers/owner/properties_controller.rb
+++ app/controllers/owner/properties_controller.rb
@@ -1,9 +1,38 @@
 module Owner
   class PropertiesController < ApplicationController
     before_action :authenticate_user!
+    before_action :set_property, only: [:edit, :update]
 
     def index
       @properties = current_user.properties
+    end
+
+    def update
+      if @property.update!(property_params)
+        redirect_to edit_owner_property, notice: 'Property updated successfully'
+      else
+        redirect_back fallback_location: edit_owner_property, alert: 'Failed to update property'
+      end
+    end
+
+    private
+
+    def set_property
+      @property = current_user.properties.find(params[:id])
+    end
+
+    def property_params
+      params.require(:property).permit(
+        :name,
+        :price,
+        :headine,
+        :description,
+        :address_1,
+        :address_2,
+        :city,
+        :state,
+        :country_code
+      )
     end
   end
 end
